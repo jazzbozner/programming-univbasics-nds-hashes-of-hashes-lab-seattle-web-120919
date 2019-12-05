20a74641ccf4ac52b35bc45f401000528e30d653
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 21:33:44 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,6 +46,12 @@ def naming_system
             sub_category: {
               label: "Species",
               sub_category: nil}
+          }
+        }
+      }
+    }
+  }
+  }
   
   # Remember:
   #  Kingdom
