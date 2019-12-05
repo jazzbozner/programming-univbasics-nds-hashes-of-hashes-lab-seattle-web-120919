15ba1a61dd8d318dcb6546142a3d68e180d0f679
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 21:32:33 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,15 +31,28 @@
 # }
 
 def naming_system
-  label: "Kingdom",
+ {label: "Kingdom",
   sub_category: {
     label: "Phylum",
     sub_category: {
-      label: "Class"
+      label: "Class",
       sub_category: {
-        label: "Order"
-        
+        label: "Order",
+        sub_category: {
+          label: "Family",
+          sub_category: {
+            label: "Genus",
+            sub_category: {
+              label: "Species",
+              sub_category: nil}
+          }
         }
+      }
+    }
+  }
+ }
+              
+           
 
       label: "Family",
     label: "Genus",
